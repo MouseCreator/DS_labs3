ea9358dce738a84a9c9b795f828f761373fbebfe
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-29 19:58:32 +0300

    implemented barbershop problem in go

--- lab3/Go/barber.go
+++ lab3/Go/barber.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+type Customer struct {
+	id int
+}
+
+type Barbershop struct {
+	barbershopQueue chan int
+	doneChannel     chan bool
+	haircutsChannel chan int
+}
+
+func (c Customer) getHaircut(barbershop *Barbershop, group *sync.WaitGroup) {
+	barbershop.barbershopQueue <- c.id
+	fmt.Printf("%d goes to barber's chair\n", c.id)
+	<-barbershop.haircutsChannel
+	fmt.Printf("%d gets its haircut and leaves barbershop\n", c.id)
+	group.Done()
+}
+
+func (b Barbershop) doBarber() {
+	id := 0
+	for {
+		select {
+		case id = <-b.barbershopQueue:
+			fmt.Printf("customer arrived %d\n", id)
+			b.haircutsChannel <- id
+		case <-b.doneChannel:
+			return
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	barbershop := Barbershop{make(chan int), make(chan bool), make(chan int)}
+
+	customers := make([]Customer, 10)
+	for i := 0; i < len(customers); i++ {
+		customers[i] = Customer{i + 1}
+	}
+	group := sync.WaitGroup{}
+	group.Add(10)
+	go barbershop.doBarber()
+	for _, c := range customers {
+		go c.getHaircut(&barbershop, &group)
+	}
+	group.Wait()
+	barbershop.doneChannel <- true
+
 }
