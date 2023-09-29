bbed234083cc88c0feea2630fb577c9414ad5602
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-09-29 18:21:13 +0300

    added custom semaphore interface

---
+++ lab3/Lab3/src/main/java/org/example/winnie/Pot.java
@@ -0,0 +1,10 @@
+package org.example.winnie;
+
+public class Pot {
+    private final int sizeLimit;
+    private int sizeFilled;
+    public Pot(int sizeLimit) {
+        this.sizeLimit = sizeLimit;
+        sizeFilled = 0;
+    }
+}
---
+++ lab3/Lab3/src/main/java/org/example/winnie/semaphore/CustomSemaphore.java
@@ -0,0 +1,4 @@
+package org.example.winnie.semaphore;
+
+public interface CustomSemaphore {
+}
