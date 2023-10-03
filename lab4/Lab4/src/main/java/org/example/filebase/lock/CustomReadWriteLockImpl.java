8abdcb1409310fa41bf73a910012fdee9b418542
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-04 02:51:45 +0300

    added custom simple read/write lock implementation

---
+++ lab4/Lab4/src/main/java/org/example/filebase/lock/CustomReadWriteLockImpl.java
@@ -0,0 +1,67 @@
+package org.example.filebase.lock;
+
+public class CustomReadWriteLockImpl implements CustomReadWriteLock {
+
+    private final ReadLock readLock = new ReadLock();
+    private final WriteLock writeLock = new WriteLock();
+    @Override
+    public CustomLock readLock() {
+        return readLock;
+    }
+
+    @Override
+    public CustomLock writeLock() {
+        return writeLock;
+    }
+
+    private final Object writeSync = new Object();
+    private final Object readSync = new Object();
+    private int readEntered = 0;
+    private boolean writeLocked = false;
+    private class ReadLock implements CustomLock {
+
+        @Override
+        public void lock() throws InterruptedException {
+            synchronized (readSync) {
+                ++readEntered;
+            }
+            synchronized (writeSync) {
+                while (writeLocked)
+                    writeSync.wait();
+                writeLocked = true;
+            }
+        }
+
+        @Override
+        public void unlock() {
+            synchronized (readSync) {
+                readEntered--;
+                synchronized (writeSync) {
+                    if (readEntered == 0) {
+                        writeSync.notifyAll();
+                    }
+                }
+            }
+        }
+    }
+
+    private class WriteLock implements CustomLock {
+
+        @Override
+        public void lock() throws InterruptedException {
+            synchronized (writeSync) {
+                while (writeLocked)
+                    writeSync.wait();
+                writeLocked = true;
+            }
+        }
+
+        @Override
+        public void unlock() {
+            synchronized (writeSync) {
+                writeLocked = false;
+                writeSync.notifyAll();
+            }
+        }
+    }
+}
