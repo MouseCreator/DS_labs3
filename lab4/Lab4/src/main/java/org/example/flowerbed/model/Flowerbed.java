22c2e5e0afdbb5067714f642024f3be2975b0f37
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-04 06:01:28 +0300

    added Flowerbed manager

--- lab4/Lab4/src/main/java/org/example/flowerbed/model/Flowerbed.java
+++ lab4/Lab4/src/main/java/org/example/flowerbed/model/Flowerbed.java
@@ -1,10 +1,11 @@
 package org.example.flowerbed.model;
 
-public class Flowerbed {
+import java.util.Iterator;
+
+public class Flowerbed implements Iterable<Flower> {
     private Flower[][] flowers;
 
     public Flowerbed() {
-
     }
 
     public void initArray(int N, int M) {
@@ -21,4 +22,31 @@ public class Flowerbed {
     public Flower[][] getFlowers() {
         return flowers;
     }
+
+    @Override
+    public Iterator<Flower> iterator() {
+        return new FlowerIterator();
+    }
+
+    private class FlowerIterator implements Iterator<Flower> {
+        private int row = 0;
+        private int col = 0;
+
+        @Override
+        public boolean hasNext() {
+            return row < flowers.length && col < flowers[row].length;
+        }
+
+        @Override
+        public Flower next() {
+            Flower flower = flowers[row][col];
+            col++;
+            if (col >= flowers[row].length) {
+                col = 0;
+                row++;
+            }
+            return flower;
+        }
+    }
+
 }
---
+++ lab4/Lab4/src/main/java/org/example/flowerbed/model/FlowerbedManager.java
@@ -0,0 +1,82 @@
+package org.example.flowerbed.model;
+
+import org.example.filebase.manager.FileManager;
+import org.example.filebase.manager.FileManagerImpl;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class FlowerbedManager {
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    private final Flowerbed flowerbed = new Flowerbed();
+
+    private final FileManager fileManager = new FileManagerImpl();
+
+    public void waterFlowers() {
+        try {
+            readWriteLock.writeLock().lock();
+
+            for (Flower flower : flowerbed) {
+                if (flower.getState().equals(Flower.State.WITHERED)) {
+                    flower.setState(Flower.State.GROWING);
+                }
+            }
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+    }
+
+    public void modifyFlowers() {
+        try {
+            readWriteLock.writeLock().lock();
+
+            for (Flower flower : flowerbed) {
+                if (flower.getState().equals(Flower.State.WITHERED)) {
+                    flower.setState(Flower.State.DEAD);
+                } else if (flower.getState().equals(Flower.State.DEAD)) {
+                    flower.setState(Flower.State.GROWING);
+                } else {
+                    flower.setState(Flower.State.WITHERED);
+                }
+            }
+        } finally {
+            readWriteLock.writeLock().unlock();
+        }
+
+    }
+
+    public void printFlowers() {
+        try {
+            readWriteLock.readLock().lock();
+            String s = getFlowerStates();
+            System.out.println(s);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+    }
+
+    public void writeFlowersToFile(String file) {
+        try {
+            readWriteLock.readLock().lock();
+            String s = getFlowerStates();
+            fileManager.append(file, s);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            readWriteLock.readLock().unlock();
+        }
+    }
+
+    private String getFlowerStates() {
+        StringBuilder builder = new StringBuilder();
+        for (Flower[] flowerRow : flowerbed.getFlowers()) {
+            for (Flower flower : flowerRow) {
+                builder.append(flower.print()).append(" ");
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+
+    }
+}
