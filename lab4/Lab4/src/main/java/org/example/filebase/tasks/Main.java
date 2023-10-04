a7d9f23ae9216e3e4ddbbdcfc0bd5caac61a6a13
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-05 01:24:07 +0300

    added clear file method

--- lab4/Lab4/src/main/java/org/example/filebase/tasks/Main.java
+++ lab4/Lab4/src/main/java/org/example/filebase/tasks/Main.java
@@ -1,15 +1,24 @@
 package org.example.filebase.tasks;
 
+import org.example.filebase.manager.FileManager;
+import org.example.filebase.manager.FileManagerImpl;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Threads will run until ENTER is pressed");
+        clearFile();
         TaskManager taskManager = new TaskManager();
         taskManager.startThreads();
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         taskManager.stopThreads();
         scanner.close();
+    }
+
+    private static void clearFile() {
+        FileManager fileManager = new FileManagerImpl();
+        fileManager.write("src/main/resources/file/database.txt", "");
     }
 }
