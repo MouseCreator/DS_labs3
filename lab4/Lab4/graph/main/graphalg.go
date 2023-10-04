33ced9e10ce52272d3c3389f2ce6c63fe81d1225
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-04 22:21:30 +0300

    implemented add/delete node to graph functions

--- lab4/Lab4/graph/main/graphalg.go
+++ lab4/Lab4/graph/main/graphalg.go
@@ -5,8 +5,55 @@ import (
 	"sync"
 )
 
+type Node struct {
+	id string
+}
+
+type Edge struct {
+	to     *Node
+	weight int
+}
+
+func (g *Graph) addNode(id string) {
+	node := Node{id}
+	edges := make([]Edge, 0)
+	g.nodesMap[node] = edges
+}
+
+func (g *Graph) removeNode(id string) {
+	for node, edges := range g.nodesMap {
+		if node.id == id {
+			for _, v := range edges {
+				inc := v.to
+				edges2 := g.nodesMap[*inc]
+				edges2 = remove(edges2, node)
+				g.nodesMap[*inc] = edges
+				return
+			}
+			delete(g.nodesMap, node)
+			return
+		}
+	}
+}
+
+func removeEdgeIndex(s []Edge, index int) []Edge {
+	ret := make([]Edge, 0)
+	ret = append(ret, s[:index]...)
+	return append(ret, s[index+1:]...)
+}
+
+func remove(edges []Edge, node Node) []Edge {
+	for i, edge := range edges {
+		if edge.to == &node {
+			return removeEdgeIndex(edges, i)
+		}
+	}
+	return edges
+}
+
 type Graph struct {
-	mutex sync.RWMutex
+	mutex    sync.RWMutex
+	nodesMap map[Node][]Edge
 }
 
 func main() {
