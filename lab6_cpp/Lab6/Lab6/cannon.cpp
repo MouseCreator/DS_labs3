6858741c32b2e1ab5f72d78eb9012ce80b52706c
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-22 18:40:02 +0300

    added main function, implemented fox algorithm

--- lab6_cpp/Lab6/Lab6/cannon.cpp
+++ lab6_cpp/Lab6/Lab6/cannon.cpp
@@ -5,8 +5,8 @@ void calculate(double* AMatrix, double* BMatrix, double* ABlock, double* BBlock)
 	int lim = psq - 1;
 	for (int i = 0; i < lim; ++i) {
 		for (int j = 0; j < lim; ++j) {
-			sendBlock(ABlock, i, (j - i + psq % psq));
-			sendBlock(BBlock, (j - i + psq % psq), j);
+			//sendBlock(ABlock, i, (j - i + psq % psq));
+			//sendBlock(BBlock, (j - i + psq % psq), j);
 		}
 	}
 }
--- lab6_cpp/Lab6/Lab6/main.cpp
+++ lab6_cpp/Lab6/Lab6/main.cpp
@@ -1,2 +1,7 @@
-
+#include "fox.h"
+#include "tape.h"
 //https://github.com/microsoft/Microsoft-MPI/blob/master/src/include/mpi.h
+
+void main(int argc, char* argv[]) {
+	Tape::runTapeMultiplication(argc, argv);
+}
