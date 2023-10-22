1cdd640b92d3dce802d3e64a45f0422f358a7cac
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-22 20:27:03 +0300

    added communicators

--- lab6_cpp/Lab6/Lab6/cannon.h
+++ lab6_cpp/Lab6/Lab6/cannon.h
@@ -4,6 +4,7 @@ namespace Cannon {
 
 	int GridCoords[2];
 	int GridSize;
+	MPI_Comm GridComm;
 
 	void calculate(double* AMatrix, double* BMatrix, double* ABlock, double* BBlock) {
 		int psq = sqrt(ProcNum);
@@ -30,7 +31,22 @@ namespace Cannon {
 		receiveBlock(pAMatrix, pABlock, N, (N + M) % GridSize, Size, BlockSize, RowComm);
 		receiveBlock(pAMatrix, pABlock, (N + M) % GridSize, M, Size, BlockSize, ColComm);
 	}
-	void initialDistribution(double* AMatrix, double* BMatrix, double* CMatrix, int Size, int BlockSize) {
-		
+
+	void CreateGridCommunicators() {
+		int DimSize[2];
+		int Periodic[2];
+		int Subdims[2]; 
+		DimSize[0] = GridSize;
+		DimSize[1] = GridSize;
+		Periodic[0] = 0;
+		Periodic[1] = 0;
+		MPI_Cart_create(MPI_COMM_WORLD, 2, DimSize, Periodic, 1, &GridComm);
+		MPI_Cart_coords(GridComm, ProcRank, 2, GridCoords);
+		Subdims[0] = 0; 
+		Subdims[1] = 1; 
+		MPI_Cart_sub(GridComm, Subdims, &RowComm);
+		Subdims[0] = 1;
+		Subdims[1] = 0;
+		MPI_Cart_sub(GridComm, Subdims, &ColComm);
 	}
 }
