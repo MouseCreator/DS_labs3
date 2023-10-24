048eff3db11f5b44d18ae0d5ffdb7b261796dc2d
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-10-24 12:48:52 +0300

    added parking model in java

--- module1/Module1_Var10/src/main/java/univ/module/Main.java
+++
@@ -1,7 +0,0 @@
-package univ.module;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ module1/Module1_Var10/src/main/java/univ/module/Parking.java
@@ -0,0 +1,95 @@
+package univ.module;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parking {
+    private static class ParkingSlots {
+        private int spacesLeft;
+        private ParkingSlots(int spacesLeft) {
+            this.spacesLeft = spacesLeft;
+        }
+
+        public int getSpacesLeft() {
+            return spacesLeft;
+        }
+        public void acquire() {
+            spacesLeft--;
+        }
+        public void release() {
+            spacesLeft++;
+        }
+    }
+    private static class Car {
+
+        private final int id;
+        private static final Object sync = new Object();
+
+        private Car(int id) {
+            this.id = id;
+        }
+
+        public void acquire(ParkingSlots parking, long waitMillis, long acquireMillis) throws InterruptedException {
+            synchronized (sync) {
+                 if (parking.getSpacesLeft() > 0) {
+                     parking.acquire();
+                     System.out.printf("Car %d acquired parking spot\n", id);
+                     sync.wait(acquireMillis);
+                 } else {
+                    long endTime = System.currentTimeMillis() + waitMillis;
+                    sync.wait(waitMillis);
+                    if (System.currentTimeMillis() < endTime) {
+                        parking.acquire();
+                    } else {
+                        System.out.printf("Car %d didn't acquire parking spot\n", id);
+                        return;
+                    }
+                 }
+            }
+
+            synchronized (sync) {
+                parking.release();
+                System.out.printf("Car %d released parking spot\n", id);
+            }
+        }
+    }
+
+    private static class CarThread extends Thread {
+        private final Car car;
+        private final long timeout;
+        private final long acquire;
+        private final ParkingSlots parkingSlots;
+
+        private CarThread(Car car, long timeout, long acquire, ParkingSlots parkingSlots) {
+            this.car = car;
+            this.timeout = timeout;
+            this.acquire = acquire;
+            this.parkingSlots = parkingSlots;
+        }
+        @Override
+        public void run() {
+            try {
+                car.acquire(parkingSlots, timeout, acquire);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        List<Thread> threadList = new ArrayList<>();
+        ParkingSlots parkingSlots = new ParkingSlots(5);
+        for (int i = 0; i < 10; i++) {
+            Car car = new Car(i+1);
+            Thread thread = new CarThread(car, 3000, 5000, parkingSlots);
+            threadList.add(thread);
+            thread.start();
+        }
+        for (Thread thread : threadList) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                System.err.println("Model was interrupted");
+            }
+        }
+    }
+}
