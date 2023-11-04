75c6519694964c6d66fdacf04a2db63ff40c9ca5
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-04 22:10:18 +0200

    implemented common controller

--- Lab7/src/main/java/org/example/controller/CommonController.java
+++ Lab7/src/main/java/org/example/controller/CommonController.java
@@ -1,13 +1,63 @@
 package org.example.controller;
 
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class CommonController {
-    public boolean askBoolean(String s) {
-        System.out.println(s);
-        String answer = "yes";
-        return true;
+
+    private final Scanner scanner;
+    private final PrintStream outputStream;
+    public static CommonController consoleController() {
+        return new CommonController(System.in, System.out);
+    }
+    public CommonController(InputStream in, PrintStream out) {
+        this.scanner = new Scanner(in);
+        this.outputStream = out;
+    }
+
+    private void startRequest(String request) {
+        outputStream.println(request);
+        outputStream.print("> ");
+    }
+    public boolean askBoolean(String request) {
+        startRequest(request);
+        String input = scanner.nextLine().trim().toLowerCase();
+        while (!input.equals("true") && !input.equals("false")) {
+            startRequest("Boolean value (true/false) is expected!");
+            input = scanner.nextLine().trim().toLowerCase();
+        }
+        return Boolean.parseBoolean(input);
+    }
+
+    public int askInteger(String request) {
+        startRequest(request);
+        while (true) {
+            try {
+                return Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                startRequest("Integer is expected!");
+            }
+        }
+    }
+
+    public long askLong(String request) {
+        startRequest(request);
+        while (true) {
+            try {
+                return Long.parseLong(scanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                startRequest("Long integer is expected!");
+            }
+        }
+    }
+
+    public String askString(String request) {
+        startRequest(request);
+        return scanner.nextLine().trim();
     }
 
     public void print(String s) {
-        System.out.println(s);
+        outputStream.println(s);
     }
 }
