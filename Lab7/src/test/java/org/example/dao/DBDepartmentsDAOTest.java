de771b63ece2fc0abab53047d8061d855c9e85a0
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-05 18:33:05 +0200

    added find all test

--- Lab7/src/test/java/org/example/dao/DBDepartmentsDAOTest.java
+++ Lab7/src/test/java/org/example/dao/DBDepartmentsDAOTest.java
@@ -1,5 +1,7 @@
 package org.example.dao;
 
+import org.example.extra.TestDataGenerator;
+import org.example.extra.TestHelper;
 import org.example.model.Department;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,10 +30,24 @@ class DBDepartmentsDAOTest {
             throw new RuntimeException(e);
         }
     }
+    private void withTestData() {
+        TestDataGenerator testDataGenerator = new TestDataGenerator();
+        List<Department> departmentList = testDataGenerator.allDepartments().getDepartmentList();
+        for (Department department : departmentList) {
+            dao.create(department);
+        }
+    }
 
+    private List<Department> getTestData() {
+        TestDataGenerator testDataGenerator = new TestDataGenerator();
+        return testDataGenerator.allDepartments().getDepartmentList();
+    }
     @Test
     void findAll() {
-
+        withTestData();
+        List<Department> testData = getTestData();
+        List<Department> allDepartments = dao.findAll();
+        TestHelper.compareList(testData, allDepartments);
     }
 
     @Test
