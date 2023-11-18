72cb71ff646a49a8777bad45a9744af52bfa2f5d
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-18 14:47:58 +0200

    implemented get method

--- Lab7/src/main/java/org/example/server/CommonServerController.java
+++ Lab7/src/main/java/org/example/server/CommonServerController.java
@@ -2,12 +2,17 @@ package org.example.server;
 
 import org.example.controller.DepartmentController;
 import org.example.controller.EmployeesController;
+import org.example.controller.ModelController;
+import org.example.dao.IdIterable;
 import org.example.model.Department;
 import org.example.model.Departments;
 import org.example.model.Employee;
 import org.example.model.dto.Request;
 import org.example.model.dto.Response;
 import org.example.model.dto.Status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommonServerController {
     private DepartmentController departmentController;
@@ -27,7 +32,7 @@ public class CommonServerController {
                 postMethod(request, response);
             }
             case "GET" -> {
-                //get
+                getMethod(request, response);
             }
             case "DELETE" -> {
                 //delete
@@ -68,5 +73,62 @@ public class CommonServerController {
             response.setStatus(Status.WARNING);
         }
         response.setDetailsString(details);
+    }
+    private <T extends IdIterable> List<T> getModel(ModelController<T> controller, String request) {
+        List<T> result = new ArrayList<>();
+        if (request.isEmpty()) {
+            result = controller.getAll();
+        } else {
+            request = request.replace(" +", " ");
+            String[] parts = request.split(" ", 2);
+            String cmd = formatCommand(parts[0]);
+            switch (cmd) {
+                case "a", "all" -> result = controller.getAll();
+                case "f", "filter" -> {
+                    result = controller.filter(parts[1]);
+                }
+                case "id" -> {
+                    Long id = Long.parseLong(parts[1]);
+                    T model = controller.get(id);
+                    result.add(model);
+                }
+                default -> {
+                    throw new IllegalArgumentException("Cannot resolve " + cmd);
+                }
+            }
+        }
+        return result;
+    }
+
+    private String formatCommand(String part) {
+        return part.trim().toLowerCase();
+    }
+
+    private void getMethod(Request request, Response response) {
+        Departments departments = new Departments();
+        response.setDepartments(departments);
+        String details = request.getDetails();
+        if (request.getTarget().equals("Department")) {
+            try {
+                List<Department> modelList = getModel(departmentController, details);
+                departments.setDepartmentList(modelList);
+                response.setStatus(Status.SUCCESS);
+            } catch (Exception e) {
+                response.setDetailsString(e.getMessage());
+                response.setStatus(Status.SERVER_ERROR);
+            }
+        } else if (request.getTarget().equals("Employee")) {
+            try {
+                List<Employee> modelList = getModel(employeesController, details);
+                departments.setEmployeeList(modelList);
+                response.setStatus(Status.SUCCESS);
+            } catch (Exception e) {
+                response.setDetailsString(e.getMessage());
+                response.setStatus(Status.SERVER_ERROR);
+            }
+        } else {
+            response.setDetailsString("Unknown target: " + request.getTarget());
+            response.setStatus(Status.CLIENT_ERROR);
+        }
     }
 }
