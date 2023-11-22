71e42d423d61f6997f9a590300e1eafa16fc3699
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-11-23 01:20:57 +0200

    added daemon thread

--- Lab7/src/main/java/org/example/activemq/Main.java
+++ Lab7/src/main/java/org/example/activemq/Main.java
@@ -3,6 +3,7 @@ package org.example.activemq;
 public class Main {
     public static void main(String[] args) {
         Thread serverThread = new Thread(ServerMQMain::createServer);
+        serverThread.setDaemon(true);
         serverThread.start();
         serverThread.setName("SERVER Thread");
         ClientMQMain.createClient();
