eec57acc832972656b76065116627b67a94cd22f
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-12-16 18:49:40 +0200

    created boot project added employee model

---
+++ Lab9/Lab9_Boot/src/main/java/univ/lab/lab9_boot/Lab9BootApplication.java
@@ -0,0 +1,13 @@
+package univ.lab.lab9_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab9BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab9BootApplication.class, args);
+    }
+
+}
