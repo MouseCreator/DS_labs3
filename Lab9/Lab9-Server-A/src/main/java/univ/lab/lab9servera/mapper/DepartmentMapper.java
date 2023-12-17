3156f49f22b4573c3146adcd8e0864b5034eba3c
Mykhailo Tyshchenko <87854427+MouseCreator@users.noreply.github.com>
2023-12-17 19:22:28 +0200

    improved mapping

--- Lab9/Lab9-Server-A/src/main/java/univ/lab/lab9servera/mapper/DepartmentMapper.java
+++ Lab9/Lab9-Server-A/src/main/java/univ/lab/lab9servera/mapper/DepartmentMapper.java
@@ -1,6 +1,7 @@
 package univ.lab.lab9servera.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 import univ.lab.lab9servera.config.MapperConfig;
 import univ.lab.lab9servera.dto.*;
 import univ.lab.lab9servera.model.Department;
@@ -10,4 +11,13 @@ public interface DepartmentMapper {
     DepartmentResponseDTO toResponse(Department department);
     Department toDepartment(DepartmentCreateDTO createDTO);
     Department toDepartment(DepartmentUpdateDTO createDTO);
+
+    @Named("departmentById")
+    default Department getDepartmentById(Long id) {
+        return id == null ? null : new Department(id);
+    }
+    @Named("departmentToId")
+    default Long getDepartmentId(Department department) {
+        return department == null ? null : department.getId();
+    }
 }
--- Lab9/Lab9-Server-A/src/main/java/univ/lab/lab9servera/mapper/EmployeeMapper.java
+++ Lab9/Lab9-Server-A/src/main/java/univ/lab/lab9servera/mapper/EmployeeMapper.java
@@ -1,20 +1,19 @@
 package univ.lab.lab9servera.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import univ.lab.lab9servera.config.MapperConfig;
 import univ.lab.lab9servera.dto.EmployeeCreateDTO;
 import univ.lab.lab9servera.dto.EmployeeResponseDTO;
-import univ.lab.lab9servera.model.Department;
+import univ.lab.lab9servera.dto.EmployeeUpdateDTO;
 import univ.lab.lab9servera.model.Employee;
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, uses = DepartmentMapper.class)
 public interface EmployeeMapper {
-
+    @Mapping(target = "departmentId", source = "department", qualifiedByName = "departmentToId")
     EmployeeResponseDTO toResponse(Employee employee);
+    @Mapping(target = "department", source = "departmentId", qualifiedByName = "departmentById")
     Employee toEmployee(EmployeeCreateDTO createDTO);
-    default Department getDepartmentFromId(Long id) {
-        return id == null ? null : new Department(id);
-    }
-    default Long getDepartmentId(Department department) {
-        return department == null ? null : department.getId();
-    }
+    @Mapping(target = "department", source = "departmentId", qualifiedByName = "departmentById")
+    Employee toEmployee(EmployeeUpdateDTO createDTO);
+
 }
